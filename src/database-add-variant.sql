df05e431b2f490c7e9e10c7a14d04f37d25a843c
varankeerthi-dev <varankeerthi@gmail.com>
2026-02-25 15:39:09 +0530

    Update database-add-variant.sql

--- src/database-add-variant.sql
+++ src/database-add-variant.sql
@@ -8,6 +8,7 @@ ALTER TABLE material_inward ADD COLUMN IF NOT EXISTS variant_id UUID REFERENCES 
 -- Material Inward Items table
 ALTER TABLE material_inward_items ADD COLUMN IF NOT EXISTS material_id UUID REFERENCES materials(id);
 ALTER TABLE material_inward_items ADD COLUMN IF NOT EXISTS variant_id UUID REFERENCES company_variants(id);
+ALTER TABLE material_inward_items ADD COLUMN IF NOT EXISTS warehouse_id UUID;
 
 -- Material Outward table
 ALTER TABLE material_outward ADD COLUMN IF NOT EXISTS warehouse_id UUID;
