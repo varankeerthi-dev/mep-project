28f993d70c85578a5008e0bc9e1787692bd6f031
varankeerthi-dev <varankeerthi@gmail.com>
2026-02-26 05:52:54 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://rujqejtisqermjyqqgoj.supabase.co'
-const supabaseKey = 'sb_publishable_xcLv9r4hTNLEFd7TqvmNow_HDLojCpa'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1anFlanRpc3Flcm1qeXFxZ29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2OTA0NDIsImV4cCI6MjA4NzI2NjQ0Mn0.XKS-L4usiB0kKMz2p6SazFyNKc2Iyk2b35qqAoiRHio'
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
