6dd34676bf702567e9c90e64750d9e035911ea99
varankeerthi-dev <varankeerthi@gmail.com>
2026-04-01 06:00:38 +0530

    chore: update config and add instrument file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "sss-q1", 
+      project: "javascript-react",
+      // If you're running local, make sure you've run `vercel env pull` 
+      // or set this token in your terminal.
+      authToken: process.env.SENTRY_AUTH_TOKEN, 
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -13,6 +23,13 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    // --- ADD THIS FOR SENTRY PROFILING ---
+    headers: {
+      "Document-Policy": "js-profiling",
+    },
+  },
+  build: {
+    sourcemap: true, 
   }
 })
