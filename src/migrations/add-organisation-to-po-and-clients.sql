98214d92d14990fef3187d9f1b7b78a27f38c890
varankeerthi-dev <varankeerthi@gmail.com>
2026-04-20 11:58:28 +0530

    Fix migration: Add DROP POLICY IF EXISTS for new policy names to handle re-runs

--- src/migrations/add-organisation-to-po-and-clients.sql
+++ src/migrations/add-organisation-to-po-and-clients.sql
@@ -15,6 +15,7 @@ CREATE INDEX IF NOT EXISTS idx_clients_organisation_id ON clients(organisation_i
 
 -- Update RLS policies for client_purchase_orders
 DROP POLICY IF EXISTS "Enable all access for client_purchase_orders" ON client_purchase_orders;
+DROP POLICY IF EXISTS "Enable organisation access for client_purchase_orders" ON client_purchase_orders;
 CREATE POLICY "Enable organisation access for client_purchase_orders" ON client_purchase_orders
   FOR ALL TO authenticated 
   USING (organisation_id IN (SELECT organisation_id FROM user_organisations WHERE user_id = auth.uid()))
@@ -22,6 +23,7 @@ CREATE POLICY "Enable organisation access for client_purchase_orders" ON client_
 
 -- Update RLS policies for clients
 DROP POLICY IF EXISTS "Enable all access" ON clients;
+DROP POLICY IF EXISTS "Enable organisation access for clients" ON clients;
 CREATE POLICY "Enable organisation access for clients" ON clients
   FOR ALL TO authenticated 
   USING (organisation_id IN (SELECT organisation_id FROM user_organisations WHERE user_id = auth.uid()))
@@ -32,6 +34,7 @@ DROP POLICY IF EXISTS "Enable read for authenticated" ON materials;
 DROP POLICY IF EXISTS "Enable insert for authenticated" ON materials;
 DROP POLICY IF EXISTS "Enable update for authenticated" ON materials;
 DROP POLICY IF EXISTS "Enable delete for authenticated" ON materials;
+DROP POLICY IF EXISTS "Enable organisation access for materials" ON materials;
 CREATE POLICY "Enable organisation access for materials" ON materials
   FOR ALL TO authenticated 
   USING (organisation_id IN (SELECT organisation_id FROM user_organisations WHERE user_id = auth.uid()))
